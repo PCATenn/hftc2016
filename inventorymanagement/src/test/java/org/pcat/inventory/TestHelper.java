037b074b28b08fa2e6d6018e45ae8b336dbad19a
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-28 11:58:57 -0500

    commiting the TestHelper classs

---
+++ inventorymanagement/src/test/java/org/pcat/inventory/TestHelper.java
@@ -0,0 +1,35 @@
+package org.pcat.inventory;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
+public class TestHelper {
+
+	private Level saveRootLoggerLevel = null;
+	private Level testRootLoggerLevel = null;
+
+	public TestHelper() {
+		super();
+	}
+
+	public Level getRootTestLoggerLevel() {
+		return testRootLoggerLevel;
+	}
+
+	public void resetSaveRootLoggerLevel() {
+		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		root.setLevel(saveRootLoggerLevel);
+	}
+
+	public void saveCurrentRootLogging() {
+		saveRootLoggerLevel = ((Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).getLevel();
+	}
+
+	public void setRootTestLoggerLevel(Level level) {
+		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		root.setLevel(level);
+	}
+
+}
